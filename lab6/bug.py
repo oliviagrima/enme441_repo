2420e30d8fe792cd25f6cd00719d7c35292ae7ad
oliviagrima <100474858@alumnos.uc3m.es>
2025-10-19 18:24:34 -0400

    bug

--- lab6/bug.py
+++ lab6/bug.py
@@ -14,7 +14,6 @@ class Bug:
         self.__running = False
         self.__thread = None
 
-    # Método privado que hace el movimiento
     def __move(self):
         while self.__running:
             byte = 1 << self.x
@@ -24,7 +23,7 @@ class Bug:
             self.x += move
 
             if self.isWrapOn:
-                self.x %= 8  # envuelve de 0 a 7
+                self.x %= 8
             else:
                 if self.x < 0:
                     self.x = 0
@@ -33,17 +32,60 @@ class Bug:
 
             time.sleep(self.timestep)
 
-    # Método público para iniciar el bug
     def start(self):
         if not self.__running:
             self.__running = True
             self.__thread = threading.Thread(target=self.__move)
             self.__thread.start()
 
-    # Método público para detener el bug
     def stop(self):
         self.__running = False
         if self.__thread:
             self.__thread.join()
-        self.__shifter.shiftByte(0)  # apaga todos los LEDs
+        self.__shifter.shiftByte(0)
+        GPIO.cleanup()
+
+
+# Bloque ejecutable para controlar el Bug con switches
+if __name__ == "__main__":
+    # Pines de los switches
+    s1_pin = 17  # enciende/apaga Bug
+    s2_pin = 27  # cambia wrapping
+    s3_pin = 22  # aumenta velocidad 3x
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(s1_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(s2_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(s3_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    b = Bug()  # valores por defecto
+    prev_s2 = GPIO.input(s2_pin)
+
+    try:
+        while True:
+            s1 = GPIO.input(s1_pin)
+            s2 = GPIO.input(s2_pin)
+            s3 = GPIO.input(s3_pin)
+
+            # Encender/apagar Bug
+            if s1:
+                b.start()
+            else:
+                b.stop()
+
+            # Cambiar wrapping si s2 cambia de estado
+            if s2 != prev_s2:
+                b.isWrapOn = not b.isWrapOn
+                prev_s2 = s2
+
+            # Ajustar velocidad con s3
+            if s3:
+                b.timestep = 0.1 / 3
+            else:
+                b.timestep = 0.1
+
+            time.sleep(0.01)
+
+    except KeyboardInterrupt:
+        b.stop()
         GPIO.cleanup()
