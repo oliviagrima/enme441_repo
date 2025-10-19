53b0ccd18ad94e806c483292d9262e9e5f9c0aa8
oliviagrima <100474858@alumnos.uc3m.es>
2025-10-19 17:35:08 -0400

    camios

--- lab6/shifter.py
+++ lab6/shifter.py
@@ -6,26 +6,22 @@ GPIO.setmode(GPIO.BCM)
 dataPin, latchPin, clockPin = 23, 24, 25
 
 GPIO.setup(dataPin, GPIO.OUT)
-GPIO.setup(latchPin, GPIO.OUT, initial=0) # start latch & clock low
-GPIO.setup(clockPin, GPIO.OUT, initial=0)
+GPIO.setup(latchPin, GPIO.OUT, initial=0)  # start latch & clock low
+GPIO.setup(clockPin, GPIO.OUT, initial=0)  
 
-pattern = 0b01100110 # pattern to display
+pattern = 0b01100110        # 8-bit pattern to display on LED bar
 
-def ping(p): # ping the clock or latch pin
-    GPIO.output(p,1)
-    time.sleep(0)
-    GPIO.output(p,0)
+for i in range(8):
+  GPIO.output(dataPin, pattern & (1<<i))
+  GPIO.output(clockPin,1) 	    # ping the clock pin to shift register data
+  time.sleep(0)
+  GPIO.output(clockPin,0)
 
-def shiftByte(b): # send a byte of data to the output
-    for i in range(8):
-        GPIO.output(dataPin, b & (1<<i))
-        ping(clockPin) # add bit to register
-    ping(latchPin) # send register to output
+GPIO.output(latchPin, 1)        # ping the latch pin to send register to output
+time.sleep(0)
+GPIO.output(latchPin, 0)
 
 try:
-    while 1:
-        for i in range(2**8):
-            shiftByte(i)
-            time.sleep(0.5)
+  while 1: pass
 except:
-    GPIO.cleanup()
+  GPIO.cleanup()
