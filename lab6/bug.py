3a2dfe62bddff269a994ed2b18bc9fa127782009
oliviagrima <100474858@alumnos.uc3m.es>
2025-10-19 18:38:38 -0400

    prueba

--- lab6/bug.py
+++ lab6/bug.py
@@ -16,28 +16,22 @@ class Bug:
 
     def __move(self):
         while self.__running:
-            # Encender LED en la posición actual
             byte = 1 << self.x
             self.__shifter.shiftByte(byte)
 
-            # Mover posición en random walk
             move = random.choice([-1, 1])
             self.x += move
 
             if self.isWrapOn:
                 self.x %= 8
             else:
-                if self.x < 0:
-                    self.x = 0
-                elif self.x > 7:
-                    self.x = 7
+                self.x = max(0, min(7, self.x))
 
             time.sleep(self.timestep)
 
     def start(self):
         if not self.__running:
             self.__running = True
-            # Iniciar el hilo que mueve el LED
             self.__thread = threading.Thread(target=self.__move)
             self.__thread.start()
 
@@ -45,10 +39,9 @@ class Bug:
         if self.__running:
             self.__running = False
             self.__thread.join()
-        # Apagar LED cuando se detiene
         self.__shifter.shiftByte(0)
 
-"""
+
 # Bloque ejecutable para controlar el Bug con switches
 if __name__ == "__main__":
     # Pines de los switches
@@ -56,22 +49,21 @@ if __name__ == "__main__":
     s2_pin = 27  # cambia wrapping
     s3_pin = 22  # aumenta velocidad 3x
 
-    # Configuración de los pines como entradas con pull-down
+    # Configurar pines con pull-up interno porque los switches van a GND
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(s1_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(s2_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(s3_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(s1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(s2_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(s3_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    # Instanciar Bug con valores por defecto
     b = Bug()
     prev_s2 = GPIO.input(s2_pin)
 
     try:
         while True:
-            # Leer switches
-            s1 = GPIO.input(s1_pin)
-            s2 = GPIO.input(s2_pin)
-            s3 = GPIO.input(s3_pin)
+            # Lectura de switches (invertida porque con pull-up)
+            s1 = not GPIO.input(s1_pin)
+            s2 = not GPIO.input(s2_pin)
+            s3 = not GPIO.input(s3_pin)
 
             # Encender/apagar Bug
             if s1:
@@ -95,4 +87,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         b.stop()
         GPIO.cleanup()
-"""
